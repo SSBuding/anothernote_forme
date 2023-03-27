361b85380e2b9b8ce38ea5649cc7ee4c4bcf2a0b
SSbuding <2771576733@qq.com>
2023-03-27 14:35:39 +0800

    docs(logo/index/nav): 初步自定义文档

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -12,8 +12,8 @@ export default defineConfig({
   base: process.env.APP_BASE_PATH || '/',
 
   lang: 'zh-CN',
-  title: '茂茂物语',
-  description: '茂茂的成长之路，包含前端常用知识、源码阅读笔记、各种奇淫技巧、日常提效工具等',
+  title: 'cocoa',
+  description: '前端学习记录，包含前端常用知识、源码阅读笔记、各种奇淫技巧、日常提效工具等',
   head,
 
   lastUpdated: true,
@@ -38,11 +38,11 @@ export default defineConfig({
       label: '本页目录'
     },
 
-    socialLinks: [{ icon: 'github', link: 'https://github.com/maomao1996' }],
+    socialLinks: [{ icon: 'github', link: 'https://github.com/SSBuding' }],
 
     footer: {
-      message: '如有转载或 CV 的请标注本站原文地址',
-      copyright: 'Copyright © 2019-present maomao'
+      message: '原文档地址:https://notes.fe-mm.com/',
+      copyright: 'Copyright © 2023-present buding'
     },
 
     darkModeSwitchLabel: '外观',
